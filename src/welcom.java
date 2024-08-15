64d96bdaec96d0789cb281bd870cedceae352986
2363061-htnghia <23630761.nghia@student.iuh.edu.vn>
2024-08-15 13:48:31 +0700

    update

---
+++ src/welcom.java
@@ -0,0 +1,6 @@
+public class welcom {
+    public static void main(String[] args){
+        System.out.println("Hello Trong Nghia!");
+        System.out.println("Welcome to the DHKTPM19A");
+    }
+}
